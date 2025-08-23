feae7e0276b871e1c82aadbcb621a4b2f2035797
Miguel Ruiz <miguel@tenjotech.com>
2025-08-23 12:59:28 +0100

    using vector store

--- assistant/app/api/chat/route.ts
+++ assistant/app/api/chat/route.ts
@@ -9,7 +9,11 @@ export async function POST(req: Request) {
   const { messages }: { messages: UIMessage[] } = await req.json();
   const result = streamText({
     model: openai("gpt-4o"),
+    system:"You are a helpful assistant. Answer the user's question only from the file_search tool. This tool is only for school announcements. Dont use this tool for anything else.",
     messages: convertToModelMessages(messages),
+    tools: {
+      file_search: openai.tools.fileSearch({vectorStoreIds: ['vs_68a7a07d66fc8191b9666c3d763e6dc5']}),
+    },
   });
 
   return result.toUIMessageStreamResponse();
