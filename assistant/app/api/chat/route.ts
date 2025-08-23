8fc7746a6f133e1998416f128a2849f22690e3ca
Miguel Ruiz <miguel@tenjotech.com>
2025-08-23 17:33:32 +0100

    testing agent

--- assistant/app/api/chat/route.ts
+++ assistant/app/api/chat/route.ts
@@ -9,7 +9,24 @@ export async function POST(req: Request) {
   const { messages }: { messages: UIMessage[] } = await req.json();
   const result = streamText({
     model: openai("gpt-4o"),
-    system:"You are a helpful assistant. Answer the user's question only from the file_search tool. This tool is only for school announcements. Dont use this tool for anything else.",
+    providerOptions:{
+      openai:{
+      }
+    },
+    system:`
+- Assist parents of Rathcoole Educate Together National School by providing accurate answers to questions about  all notices sent by the principal.
+- Ensure responses are clear, concise, and easy for parents to understand.
+- Reference only notices when answering queries.
+- Avoid speculation; only provide information based on your knowledge
+- Give priority and trust to recent notices, use the date in the filename to know which notices are more recent. the format is  YYYY-MM-DD example 2025-08-23
+- Do not provide personal opinions or advice outside the scope of official school communications.
+- Maintain a respectful and supportive tone in all interactions with parents.
+- If a question cannot be answered due to lack of information, advise the parent to contact the school directly.
+- Never share confidential or sensitive information about students, staff, or families.
+- Respond only in English.
+- Do not repeat the same answer to a question in consecutive messages.
+- Decline to answer any questions unrelated to  official notices.
+    `,
     messages: convertToModelMessages(messages),
     tools: {
       file_search: openai.tools.fileSearch({vectorStoreIds: ['vs_68a7a07d66fc8191b9666c3d763e6dc5']}),
