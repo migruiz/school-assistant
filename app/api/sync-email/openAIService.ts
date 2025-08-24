49c06f7c940fb7da7b9473a7ac82401503d14f0d
Miguel Ruiz <miguel@tenjotech.com>
2025-08-24 19:15:37 +0100

    uplading to openai

--- app/api/sync-email/openAIService.ts
+++ app/api/sync-email/openAIService.ts
@@ -1,3 +1,13 @@
-export async function importEmails(email:any) {
-    
+import OpenAI, { Uploadable } from "openai";
+export async function importEmails(email: any) {
+    const client = new OpenAI();
+
+    const vector_store = await client.vectorStores.create({   // Create vector store
+        name: "Support FAQ",
+    });
+    const uploadable: Uploadable = new File([JSON.stringify(email)], "inputTEST.txt", { type: "text/plain" });
+    await client.vectorStores.files.uploadAndPoll(
+        vector_store.id,
+        uploadable
+    );
 }
