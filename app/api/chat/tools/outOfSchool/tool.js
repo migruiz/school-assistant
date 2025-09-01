9bb80aad3be96ea3ed270a7b94c0626864a7d6e9
Miguel Ruiz <miguel@tenjotech.com>
2025-09-01 17:49:43 +0100

    linking lilyes

--- app/api/chat/tools/outOfSchool/tool.js
+++ app/api/chat/tools/outOfSchool/tool.js
@@ -2,7 +2,9 @@
 import { z } from 'zod';
 import OpenAI from "openai";
 export const getOutOfSchoolTool = ({ openAIKey, childCareServicesDataVectorStoreId, afterSchoolDataVectorStoreId }) => ({
-    description: `This tool answers queries related to out of school activities, like childcare services (breakfast club) and after school programs like STEAM, chess, Dancing Clubs`,
+    description: `This tool answers queries related to out of school activities, like childcare services (breakfast club) and after school programs like STEAM, chess, Dancing Clubs
+    If user queries about Lily's then it refers to Childcare services.
+    `,
     inputSchema: z.object({
         userQuery: z.string().describe('The User query'),
         types: z.array(z.enum(["afterSchool", "childCare"])).describe('The type of out of school activity to search for. afterSchool: After School Activities like STEAM, chess, Dancing Clubs. childCare: Childcare services like breakfast club'),
