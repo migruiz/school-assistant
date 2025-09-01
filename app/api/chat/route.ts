47e5f6b7fad30d1457b731aa4506c100c1666e7c
Miguel Ruiz <miguel@tenjotech.com>
2025-09-01 16:01:09 +0100

    extracting tools

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -10,7 +10,7 @@ import {
 import { queryVectorStore } from './semanticSearchService'
 import { getFirestoreDatabase, getSchoolInfo } from './openAIDataService'
 import {queryGeneralInfoVectorStore} from './generalInfoService'
-import {searchCalendar} from './schoolCalendar'
+import getSchoolCalendarTool from './tools/schoolCalendar/tool'
 
 export async function POST(req: Request) {
   const db = await getFirestoreDatabase();
@@ -50,18 +50,7 @@ export async function POST(req: Request) {
         }),
         execute: async ({ query }) => await performSemanticSearch({ query }),
       }),
-        'school_calendar': tool({
-        description: `This tool answers all related to the school opening/closing dates and times, finish/collection times, and if there are any event that changes the normal schedule.
-        It can also provide information about upcoming school holidays (Mid-Term Break, Winter Break, Half Days, Early Finish, Early Collections, etc.) that may change the normal schedule.
-        The tool expects a search date range as parameter. You need to convert any relative dates (today, tomorrow, next week) in the user query to absolute dates.
-        Make sure to use the date range you choose falls into the School Year (2025-09-01 to 2026-06-26)
-        The tool uses the search date range to search the school calendar`,
-        inputSchema: z.object({
-          fromDate: z.string().describe('The Start Date in this format YYYY-MM-DD'),
-          toDate: z.string().describe('The End Date in this format YYYY-MM-DD'),
-        }),
-        execute: async ({ fromDate, toDate }) => await searchCalendar({openAIKey, schoolCalendar, fromDate, toDate }),
-      }),
+      'school_calendar': tool(getSchoolCalendarTool({openAIKey, schoolCalendar})),
     },
   });
   async function performSemanticSearch({ query }: { query: string }) {
