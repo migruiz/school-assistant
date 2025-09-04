90da69ced8b89b9f4e34221983b5c6c08bc02719
Miguel Ruiz <miguel@tenjotech.com>
2025-09-04 17:06:32 +0100

    moving to chroma

--- app/api/chat/tools/outOfSchool/tool.js
+++ app/api/chat/tools/outOfSchool/tool.js
@@ -1,6 +1,6 @@
 
 import { z } from 'zod';
-import OpenAI from "openai";
+import { search } from '@/lib/semanticSearch';
 export const getOutOfSchoolTool = ({ openAIKey, childCareServicesDataVectorStoreId, afterSchoolDataVectorStoreId }) => ({
     description: `This tool answers queries related to out of school activities, like childcare services (breakfast club) and after school programs like STEAM, chess, Dancing Clubs
     If user queries about Lily's then it refers to Childcare services.
@@ -10,43 +10,13 @@ export const getOutOfSchoolTool = ({ openAIKey, childCareServicesDataVectorStore
         types: z.array(z.enum(["afterSchool", "childCare"])).describe('The type of out of school activity to search for. afterSchool: After School Activities like STEAM, chess, Dancing Clubs. childCare: Childcare services like breakfast club'),
     }),
     execute: async ({ userQuery, types }) => {
-        let results = []
         if (types.includes("afterSchool")) {
-            const client = new OpenAI({ apiKey: openAIKey });
-
-            const response = await client.responses.create({
-                model: "gpt-4o-mini",
-                input: userQuery,
-                tools: [
-                    {
-                        type: "file_search",
-                        vector_store_ids: [afterSchoolDataVectorStoreId],
-                    },
-                ],
-            });
-
-            const result = response.output_text;
-
-            return result;
+            const chunks = await search({ query: userQuery, collectionName: "afterschool_t", numberOfResults: 15 });
+            return chunks;
         }
         if (types.includes("childCare")) {
-                        const client = new OpenAI({ apiKey: openAIKey });
-
-            const response = await client.responses.create({
-                model: "gpt-4o-mini",
-                input: userQuery,
-                tools: [
-                    {
-                        type: "file_search",
-                        vector_store_ids: [childCareServicesDataVectorStoreId],
-                    },
-                ],
-            });
-
-            const result = response.output_text;
-
-            return result;
+            const chunks = await search({ query: userQuery, collectionName: "childcare_t", numberOfResults: 15 });
+            return chunks;
         }
-        return results.join(" /n")
     }
 })
